e2a3fd4775c73357362fb60574c07a75d4269fe5
terek97 <kurbanmagomedovr@gmail.com>
2021-03-14 18:53:59 +0300

    Version 1.3 небольшой рефакторинг. добавил выброс исключений при ошибке ввода пользователем вместо простого вывода в консоль

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,23 @@ import java.io.InputStreamReader;
 public class Main {
 
     // Здесь происходит запуск приложения, запрос данных от пользователя
-
     public static void main(String[] args) throws IOException {
         System.out.println("Введите данные в формате: \"X действие Y\"");
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input = reader.readLine();
+        reader.close();
 
-        // отправка данных на проверку в Verify.
-        if (!(Verify.makeVerify(input))) {
-            System.out.println("Данные введены в неверном формате");
-        } else {
-            // если все верно, производим рассчет.
-            Calculator.calculate();
+        // отправка данных на проверку в Verify. Если что-то не так, выбрасывается исключение.
+        // показывается сообщение ошибки и программа завершается
+        try {
+            Verify.makeVerify(input);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
         }
+        // если все верно, производим рассчет.
+        Calculator.calculate();
     }
 }
+
--- src/Verify.java
+++ src/Verify.java
@@ -2,7 +2,6 @@ public class Verify {
 
     private static String[] elements; //здесь хранятся данные
     private static Boolean isRoman = false; //римские или арабские
-    private static Boolean result = true; //финальный результат
 
 
     //геттер для получения массива с данными
@@ -15,21 +14,16 @@ public class Verify {
         return isRoman;
     }
 
-    // главный метод, запускает все проверки. при первой ошибке дальше проверять не идет
-    public static Boolean makeVerify(String input) {
+    // главный метод, запускает все проверки. после первой ошибки выбрасывает исключение и дальше проверять не идет
+    public static void makeVerify(String input) throws Exception {
         makeList(input);
         verifyCount(elements);
-        if (!result) return false;
         verifyForIntegers(elements);
-        if (!result) return false;
         if (!isRoman) {
             verifyForPositive(elements);
             verifyForSize(elements);
         }
-        if (!result) return false;
         verifyAction(elements);
-
-        return result;
     }
 
     // создаем массив из введенных данных
@@ -38,50 +32,62 @@ public class Verify {
     }
 
     // проверка на количество элементов
-    private static void verifyCount(String[] elements) {
+    private static void verifyCount(String[] elements) throws Exception {
         if (elements.length != 3) {
-            result = false;
+            throw new Exception("Вы ввели неверное число элементов (проверьте свой ввод на наличие пробелов.)" +
+                    "Примеры правильного ввода: 1 + 2 или VI / III.");
         }
     }
 
     //проверяем на целочисленность. если к инту не приводится, проверяем может быть там римские цифры.
-    private static void verifyForIntegers(String[] elements) {
+    private static void verifyForIntegers(String[] elements) throws Exception {
         try {
             Integer.parseInt(elements[0]);
             Integer.parseInt(elements[2]);
         } catch (NumberFormatException e) {
             verifyForRomans(elements);
             if (!(isRoman)) {
-                result = false;
+                throw new Exception("Данные введены в неверном формате. " +
+                        "Калькулятор умеет работать только с целыми числами.");
             }
         }
     }
 
     // проверка на положительные числа. Запускается только для арабских цифр
-    private static void verifyForPositive(String[] elements) {
+    private static void verifyForPositive(String[] elements) throws Exception {
         if (!(Integer.parseInt(elements[0]) > 0 & Integer.parseInt(elements[2]) > 0)) {
-            result = false;
+            throw new Exception("Данные введены в неверном формате. " +
+                    "Калькулятор умеет работать только с положительными числами.");
         }
     }
 
     //проверка на размер (не больше 10). Запускается только для арабских цифр
-    private static void verifyForSize(String[] elements) {
+    private static void verifyForSize(String[] elements) throws Exception {
         if (!(Integer.parseInt(elements[0]) < 11 & Integer.parseInt(elements[2]) < 11)) {
-            result = false;
+            throw new Exception("Данные введены в неверном формате. " +
+                    "Калькулятор умеет работать только с числами от 1 до 10 включительно.");
         }
     }
 
     //проверка математического действия (только +-*/)
-    private static void verifyAction(String[] elements) {
+    private static void verifyAction(String[] elements) throws Exception {
         if (!(elements[1].equals("+") | elements[1].equals("-") | elements[1].equals("*") | elements[1].equals("/"))) {
-            result = false;
+            throw new Exception("Данные введены в неверном формате. " +
+                    "Калькулятор умеет выполнять операции сложения, вычитания, умножения и деления.");
         }
     }
 
     //проверка на римские цифры
-    private static void verifyForRomans(String[] elements) {
+    private static void verifyForRomans(String[] elements) throws Exception {
         if (Converter.getRoman().contains(elements[0]) & Converter.getRoman().contains(elements[2])) {
             isRoman = true;
+        }
+        // выбрасывает исключение если один из элементов входит в массив римских чисел, а другой нет
+        else if (Converter.getRoman().contains(elements[0]) | Converter.getRoman().contains(elements[2])) {
+            throw new Exception("Данные введены в неверном формате. " +
+                    "Калькулятор умеет работать только с арабскими или римскими цифрами одновременно, " +
+                    "при вводе пользователем строки вроде 3 + II возникнет ошибка." +
+                    "Так же калькулятор умеет работать только с числами от I до X включительно.");
         }
     }
 }
